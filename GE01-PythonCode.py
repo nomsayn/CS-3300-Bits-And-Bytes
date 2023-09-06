a7abfd6dc10529c83193b757df948479ec639407
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-09-06 10:38:05 -0600

    Had to go back a couple versions to get Darion's nameon python file.

--- GE01-PythonCode.py
+++ GE01-PythonCode.py
@@ -1,6 +1,6 @@
 print("Bits and Bytes")
 
-#Printing Emiliano and taking a screenshot
+#printing Emiliano and taking a screenshot
 print("Emiliano")
 
 #Printing Kenyou and taking a screenshot
@@ -8,3 +8,6 @@ print("Kenyou")
 
 # Testing Moving and merging branches
 print("Matthew Michela")
+
+# Printing Darion and taking a screenshot then merging
+print("Darion")
